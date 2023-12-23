51195d3a51bd90d20996a3ddebf4cdb3e394afdb
phklau <philipp.klausen@web.de>
2023-12-23 16:58:26 +0100

    main.cpp: POST request in main()

---
+++ main.cpp
@@ -0,0 +1,38 @@
+// Copyright 2023 Mensch
+
+#include <curl/curl.h>
+#include <iostream>
+
+const char* end_point = "http://localhost:8080/api/measurement";
+const char* data = "{\"sensor\":\"puls\",\"value\":112,\"timestamp\":\"12:12\"}";
+size_t print_response(void *buffer, size_t size, size_t nmemb, void *userp) {
+    std::cout << static_cast<const char*>(buffer) << std::endl;
+    return size;
+}
+
+int main() {
+    // set up curl global
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    // get handle
+    CURL *curl_handle = curl_easy_init();
+
+    // post header
+    struct curl_slist *header = nullptr;
+    header = curl_slist_append(header, "Content-Type: application/json");
+    header = curl_slist_append(header, "Accept: application/json");
+
+    // prepare post request
+    curl_easy_setopt(curl_handle, CURLOPT_URL, end_point);
+    curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, data);
+    curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, header);
+    // libcurl default prints data to the stdout, only to test
+    // curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, print_response);
+
+    // send request
+    int success = curl_easy_perform(curl_handle);
+    std::cout << "Request send" << std::endl
+              << "Returncode:" << success << std::endl;
+
+    return 0;
+}
